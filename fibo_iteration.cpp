8d392ce0d39d58ba4fdcb3ad797e09923511786b
Kirtan Shah <50762218+kirtan32@users.noreply.github.com>
2020-10-03 11:10:02 +0530

    Update fibo_iteration.cpp
    now it's better

--- fibo_iteration.cpp
+++ fibo_iteration.cpp
@@ -7,12 +7,17 @@ int main()
 {
 	int a=0,b=1,c;
 	int n,i;
+	struct timeval t1,t2;
+	struct timezone z1,z2;
+	gettimeofday(&t1,&z1);
 	cout<<"enter number here:  ";
 	cin>>n;
 	if(n==0)
 	{ }
 	else if(n==1)
 	{	cout<<"  "<<a;	}
+	else if(n==2)
+	{	cout<<"  "<<a;cout<<"  "<<b; }
 	else
 	{	cout<<"  "<<a;cout<<"  "<<b;
 		for(i=2;i<n;i++)
@@ -23,8 +28,8 @@ int main()
 			cout<<"  "<<c;
 		}
 	}
-	
+	gettimeofday(&t2,&z2);
+	cout<<"\n time :   "<<t2.tv_usec - t1.tv_usec<<"\n";
 return 0;
 }
-
 
